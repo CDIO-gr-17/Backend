8ab4453363be2bf1bdec2eb6f1693eeff5c7e388
Holmst33n <113360170+Holmst33n@users.noreply.github.com>
2024-05-02 16:02:19 +0200

    We now sanitize the incomming data

--- webshopapp/serializers.py
+++ webshopapp/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import Customer
+from .models import Customer, BasketItem, Product, Order
 
-class EventSerializer(serializers.ModelSerializer):
+class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
+        fields = '__all__'
+
+class BasketItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = BasketItem
         fields = '__all__'
--- webshopapp/views.py
+++ webshopapp/views.py
@@ -1,62 +1,41 @@
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework import generics, permissions
-from .models import Customer, BasketItem, Order
-from .serializers import EventSerializer
+from rest_framework import generics
+from .models import Order
+from .serializers import CustomerSerializer, BasketItemSerializer
 from django.db import transaction
-
-from rest_framework import generics, permissions
-from rest_framework.response import Response
-from django.db import transaction
-from .models import Customer, BasketItem, Order
-from .serializers import EventSerializer
+from rest_framework import status
 
 class EventCreateView(generics.CreateAPIView):
-    serializer_class = EventSerializer
     http_method_names = ['post']
-
+    
     def create(self, request, *args, **kwargs):
+
         with transaction.atomic():
-            customer_data = request.data['customerInfo']
-            customer = Customer.objects.create(
-                first_name=customer_data['first_name'],
-                last_name=customer_data['last_name'],
-                email=customer_data['email'],
-                address1=customer_data['address1'],
-                address2=customer_data.get('address2', ''),
-                city=customer_data['city'],
-                zip_code=customer_data['zip_code'],
-                country=customer_data['country'],
-                telephone_number=customer_data.get('telephone_number', ''),
-                order_comment=customer_data.get('order_comment', ''),
-                vat=customer_data.get('vat', ''),
-                business_name=customer_data.get('business_name', ''),
-                delivery_address1=customer_data.get('delivery_address1', ''),
-                delivery_address2=customer_data.get('delivery_address2', ''),
-                delivery_city=customer_data.get('delivery_city', ''),
-                delivery_country=customer_data.get('delivery_country', ''),
-                delivery_zip=customer_data.get('delivery_zip', '')
-            )
+            customer_serializer = CustomerSerializer(data=request.data['customerInfo'])
+            customer_serializer.is_valid(raise_exception=True)
+            customer = customer_serializer.save()
 
-            orders = []
-            basket_items_data = request.data['basketItems']
+            order = Order.objects.create(customer=customer)
+
+            basket_items_data = request.data.get('basketItems', [])
             for item in basket_items_data:
-                basket_item = BasketItem.objects.create(
-                    product_id=item['productId'],
-                    quantity=item['quantity'],
-                    rebate_percent=item['rebatePercent'],
-                    total_line_price=item['totalLinePrice'],
-                    gift_wrapping=item['giftwrapping']
-                )
-                order = Order.objects.create(
-                    customer=customer,
-                    basket_line=basket_item
-                )
-                orders.append(order)
 
-            return Response({"status": "success"})
+                item['order'] = order.order_number
+
+                print(item)
+
+                basket_item_serializer = BasketItemSerializer(data=item)
+                basket_item_serializer.is_valid(raise_exception=True)
+                basket_item_serializer.save()
+
+            return Response({
+                "status": "success",
+                "order_number": order.order_number,
+                "customer": f"{customer.first_name} {customer.last_name}",
+                "num_items": len(basket_items_data)
+            })
 
 class EventListView(generics.ListAPIView):
     #queryset = Event.objects.all()
-    serializer_class = EventSerializer
+    # serializer_class = EventSerializer
     http_method_names = ['get']
